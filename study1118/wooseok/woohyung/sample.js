67d09724e6ea834e00f19b2bd10043b7c1654579
ansdngud <dngud1013@gmail.com>
2024-11-18 15:09:53 +0900

    Update sample.js

--- study1118/wooseok/woohyung/sample.js
+++ study1118/wooseok/woohyung/sample.js
@@ -1,20 +1,20 @@
 <script>
-function compareNumbers() {
-  // input 값 가져오기
-  let a = document.getElementById('inputA').value;
-  let b = document.getElementById('inputB').value;
+  function compareNumbers() {
+    // input 값 가져오기
+    let a = document.getElementById('inputA').value;
+    let b = document.getElementById('inputB').value;
 
-  // 숫자로 변환
-  a = Number(a);
-  b = Number(b);
+    // 숫자로 변환
+    a = Number(a);
+    b = Number(b);
 
-  // if 조건문
-  if (a > b) {
-    document.getElementById('result').innerText = 'A가 B보다 큼';
-  } else if (a < b) {
-    document.getElementById('result').innerText = 'A가 B보다 작음';
-  } else {
-    document.getElementById('result').innerText = 'A와 B가 같음';
-  }
-}    
+    // if 조건문
+    if (a > b) {
+      document.getElementById('result').innerText = 'A가 B보다 큼';
+    } else if (a < b) {
+      document.getElementById('result').innerText = 'A가 B보다 작음';
+    } else {
+      document.getElementById('result').innerText = 'A와 B가 같음';
+    }
+  }    
 </script>
